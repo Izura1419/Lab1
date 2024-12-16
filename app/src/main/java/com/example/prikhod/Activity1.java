7d1d06b92507560fef9b0684f0f3f016ae80975b
Izura <58514698+Izura1419@users.noreply.github.com>
2024-12-16 08:30:01 +0300

    Initial commit

---
+++ app/src/main/java/com/example/prikhod/Activity1.java
@@ -0,0 +1,33 @@
+package com.example.prikhod;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class Activity1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        Button btn1 = findViewById(R.id.btn1);
+        btn1.setOnClickListener(v -> {
+            Intent intent = new Intent(Activity1.this, Activity2.class);
+            intent.putExtra("parameter", "Приходько"); // передаём параметр
+            startActivity(intent); // запускаем SecondActivity
+        });
+    }
+}
